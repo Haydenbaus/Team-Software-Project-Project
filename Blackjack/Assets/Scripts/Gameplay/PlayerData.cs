6cba67655bfaff8f8117a53c2a69a0f5173578d0
ArtDuncan <ajduncan@mtu.edu>
2024-10-14 00:18:19 -0400

    Created preliminary betting and money tracking

---
+++ Blackjack/Assets/Scripts/Gameplay/PlayerData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    double playerMoney = 100.00;
+
+    public double getMoney()
+    {
+        return playerMoney;
+    }
+}
